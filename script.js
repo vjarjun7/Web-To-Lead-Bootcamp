c16ca8213f49ab21f671600bf418add52fac884a
vgehani <vgehani@KIDC-VIJAYG850>
2024-04-13 14:47:50 +0530

    ReCaptcha Added

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function beforesubmit() {
   );
   outputDate.value = formattedDateValue;
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
